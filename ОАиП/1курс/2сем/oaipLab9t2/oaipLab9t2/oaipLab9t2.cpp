561ca1ace3ca1d2a91c8b8db7d00b13c0563d3cb
MariLoz <125284586+MariLoz@users.noreply.github.com>
2023-02-28 22:12:52 +0300

    git add.

---
+++ ОАиП/1курс/2сем/oaipLab9t2/oaipLab9t2/oaipLab9t2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+string DecompressStr( string S)
+{
+    int a=1, b;
+    char c, k;
+    while (a>0)
+    {
+        a = S.find('{');
+        if (a < 0)
+            break;
+        k = S[a - 1];
+        c = S[a + 1];
+        b = c - '0';
+        S.erase(a, 3);
+        S.insert(a, b - 1, k);
+        
+    }
+    return S;
+}
+
+int main()
+{
+    string S;
+    cin >> S;
+    
+    cout << DecompressStr(S);
+
+}
